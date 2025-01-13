981bad26987b1a39dc8fc86b466bb063e8dd1e75
David Carroll <david@hazeltine.co>
2025-01-13 15:17:54 -0800

    refine

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import { songTemplates } from './data/tunes';
 import './App.css';
 
 const WeemixApp = () => {
-  const [topic, setTopic] = useState('animals');
-  const [tune, setTune] = useState('tuneBuns');
+  const [topic, setTopic] = useState('food');
+  const [tune, setTune] = useState('tuneFrere');
   const [currentLyrics, setCurrentLyrics] = useState([]);
-  const [accentColor, setAccentColor] = useState('blue');
+  const [accentColor, setAccentColor] = useState('green');
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const topics = [
+    { name: '🍕 Food', value: 'food' },
     { name: '🐶 Animals', value: 'animals' },
     { name: '⚽️ Sports', value: 'sports' },
-    { name: '🍕 Food', value: 'food' },
     { name: '🌌 Star Wars', value: 'starwars' },
     { name: '👾 Minecraft', value: 'minecraft' },
     { name: '🍄 Mario', value: 'mario' },
     { name: '☁️ Pixar', value: 'pixar' },
     { name: '🧙‍♂️ Harry Potter', value: 'harry' },
     { name: '📄 The Office', value: 'office' },
-    { name: '👹 Anything', value: 'anything' }
+    { name: '👹 Anything', value: 'anything' },
   ];
 
   const tunes = [
@@ -28,17 +28,17 @@ const WeemixApp = () => {
     { name: '✨ Twinkle Twinkle', value: 'tuneTwinkle' },
     { name: '🛶 Row Boat', value: 'tuneRow' },
     { name: '🐑 Mary Lamb', value: 'tuneMary' },
-    { name: '🥐 Hot Buns', value: 'tuneBuns' }
+    { name: '🥐 Hot Cross', value: 'tuneBuns' },
   ];
 
   const colors = [
+    { name: '💚', value: 'green' },
     { name: '💙', value: 'blue' },
     { name: '💜', value: 'purple' },
     { name: '🩷', value: 'fuchsia' },
     { name: '❤️', value: 'red' },
     { name: '🧡', value: 'orange' },
     { name: '💛', value: 'yellow' },
-    { name: '💚', value: 'green' }
   ];
 
   useEffect(() => {
@@ -143,7 +143,7 @@ const WeemixApp = () => {
         {/* Sing button */}
         <div
           onClick={handleSing}
-          className="w-full flex items-center justify-center text-white bg-black xl:py-6 md:py-4 py-2 rounded-t-full whitespace-nowrap cursor-pointer"
+          className="w-full flex items-center justify-center text-white sm:text-6xl text-2xl  bg-black xl:py-8 md:py-6 py-4 md:rounded-t-[4rem] rounded-t-[2rem] whitespace-nowrap cursor-pointer"
         >
           Sing!
         </div>
@@ -160,8 +160,8 @@ const WeemixApp = () => {
         <div
           onClick={() => setIsModalOpen(true)}
           className={`
-            w-full flex items-center justify-center sm:gap-3 gap-2 rounded-full cursor-pointer
-            md:pl-10 pl-5 md:pr-8 pr-4
+            w-full flex items-center justify-center sm:gap-8 gap-1 rounded-full cursor-pointer
+            md:pr-10 pr-3 md:pl-8 pl-4
             lg:text-3xl md:text-2xl sm:text-xl text-lg
             xl:py-6 md:py-4 py-2
             ${accentColor === 'blue' ? 'bg-blue-500' :
@@ -175,10 +175,10 @@ const WeemixApp = () => {
             }
             `}
         >
-          <span className="whitespace-break-spaces md:leading-6 leading-5 text-center">
+          <span className="material-symbols-rounded md:!text-3xl !text-2xl">tune</span>
+          <span className="w-full whitespace-break-spaces md:leading-6 leading-5 text-center">
             Sing about {topics.find(t => t.value === topic)?.name} to {tunes.find(t => t.value === tune)?.name}
           </span>
-          <span className="material-symbols-rounded md:!text-3xl !text-2xl">tune</span>
         </div>
 
         {/* Mix button */}
@@ -205,8 +205,8 @@ const WeemixApp = () => {
       {/* Lyrics */}
       <div className={`
         lyrics flex flex-1 flex-col justify-evenly w-full md:p-8 p-2 overflow-y-auto
-        xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl text-2xl
-        xl:leading-12 lg:leading-10 md:leading-8 sm:leading-6 leading-5
+        xl:text-6xl lg:text-5xl md:text-4xl text-3xl
+        xl:leading-12 lg:leading-10 md:leading-9 leading-7
         ${accentColor === 'blue' ? 'text-blue-500' :
           accentColor === 'red' ? 'text-red-500' :
             accentColor === 'green' ? 'text-green-500' :
